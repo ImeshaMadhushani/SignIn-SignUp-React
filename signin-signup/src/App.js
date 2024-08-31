65645e1211cce72d49a59065991cff54e70f10e7
K.M.I.I.Madhushani <139638076+ImeshaMadhushani@users.noreply.github.com>
2024-08-31 09:26:31 +0530

    Create  basic

---
+++ signin-signup/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import { SignInSignUp } from './Components/SignInSignUP/SignInSignUp';
+
+function App() {
+  return (
+    <div >
+      <SignInSignUp/>
+    </div>
+  );
+}
+
+export default App;
